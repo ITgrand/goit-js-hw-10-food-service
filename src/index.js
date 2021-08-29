04b5b42521f07e5a10977ef5a8c927103c7fa9c3
ITgrand <vitaliidovhopolyk@gmail.com>
2021-08-29 03:26:49 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import './css/styles.css';
-import cards from './menuCards.hbs';
-import listCards from './menu.json';
+// import './css/styles.css';
+// import cards from './menuCards.hbs';
+// import ingredients from 'menu.json';
 
 
-const Theme = {
-  LIGHT: 'light-theme',
-  DARK: 'dark-theme',
-};
+// const Theme = {
+//   LIGHT: 'light-theme',
+//   DARK: 'dark-theme',
+// };
 
-const listCardsMenu = document.querySelector('js-menu');
-const listCardsItem = cards(listCards);
-listCardsMenu.insertAdjacentHTML('beforeend', listCardsItem);
+// const listCardsMenu = document.querySelector('js-menu');
+// const listCardsItem = cards(listCards);
+// listCardsMenu.insertAdjacentHTML('beforeend', listCardsItem);
+
